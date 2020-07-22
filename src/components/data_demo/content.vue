f78030e7d08485c6c5489909ccf8399fb11e6d1a
theone <xuguangyue@dtrobo.com>
2020-07-22 15:26:31 +0800

    操盘策略部分UI

--- src/components/data_demo/content.vue
+++ src/components/data_demo/content.vue
@@ -1,23 +1,93 @@
 <template>
   <div class="main-content">
-    <div class="left-chart">
-      <dv-border-box-11 class="left-chart-container" title="今日资讯">
-        <dv-decoration-7 class="left-chart-container-title">国际资讯</dv-decoration-7>
-        <dv-scroll-board class="left-chart-container-table" :config="config" />
+    <div class="chart">
+      <dv-border-box-11 class="chart-container" title="今日资讯">
+        <dv-decoration-7 class="chart-container-left-title">国际资讯</dv-decoration-7>
+        <dv-scroll-board class="chart-container-left-table" :config="config" />
 
-        <dv-decoration-7 class="left-chart-container-title">国家资讯</dv-decoration-7>
-        <dv-scroll-board class="left-chart-container-table" :config="config" />
+        <dv-decoration-7 class="chart-container-left-title">国家资讯</dv-decoration-7>
+        <dv-scroll-board class="chart-container-left-table" :config="config" />
 
-        <dv-decoration-7 style="width: 140px" class="left-chart-container-title">关注板块资讯</dv-decoration-7>
-        <dv-scroll-board class="left-chart-container-table" :config="config" />
+        <dv-decoration-7 style="width: 140px" class="chart-container-left-title">关注板块资讯</dv-decoration-7>
+        <dv-scroll-board class="chart-container-left-table" :config="config" />
 
-        <dv-decoration-7 class="left-chart-container-title">个人资讯</dv-decoration-7>
-        <dv-scroll-board class="left-chart-container-table" :config="config" />
+        <dv-decoration-7 class="chart-container-left-title">个人资讯</dv-decoration-7>
+        <dv-scroll-board class="chart-container-left-table" :config="config" />
       </dv-border-box-11>
-      <dv-border-box-11 class="left-chart-container" title="今日仓位">2</dv-border-box-11>
-      <dv-border-box-11 class="left-chart-container" title="今日操盘策略">
-        <dv-decoration-11 style="width:150px;height:60px;">策略一</dv-decoration-11>
-        <dv-decoration-9 style="width:150px;height:150px;">策略二</dv-decoration-9>
+      <dv-border-box-11 class="chart-container" title="今日仓位">2</dv-border-box-11>
+      <dv-border-box-11 class="chart-container" title="今日操盘策略">
+        <div class="chart-container-right">
+          <dv-decoration-9 class="chart-container-right-strategy">策略一</dv-decoration-9>
+          <div class="chart-container-right-con">
+            <div class="chart-container-right-con-item">
+              <div>当前持仓</div>
+              <div>
+                <div>概念板块：贵州茅台[600519]、山西汾酒[600809]、 五粮液[000858]</div>
+                <div>军工板块：航天晨光[600501]、东安动力[600178]、航发科技[600391]...</div>
+                <div>军工板块：航天晨光[600501]、东安动力[600178]、航发科技[600391]...</div>
+              </div>
+            </div>
+            <div class="chart-container-right-con-item">
+              <div>今日新增</div>
+              <div>
+                <div>基建板块：中国交建[601800]、中国建筑[601668]、中国铁建[601186]</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <dv-decoration-2 class="chart-container-line" />
+        <div class="chart-container-right">
+          <dv-decoration-9 class="chart-container-right-strategy">策略二</dv-decoration-9>
+          <div class="chart-container-right-con">
+            <div class="chart-container-right-con-item">
+              <div>当前持仓</div>
+              <div>
+                <div>概念板块：贵州茅台[600519]、山西汾酒[600809]、 五粮液[000858]</div>
+                <div>军工板块：航天晨光[600501]、东安动力[600178]、航发科技[600391]...</div>
+              </div>
+            </div>
+            <div class="chart-container-right-con-item">
+              <div>今日新增</div>
+              <div>
+                <div>基建板块：中国交建[601800]、中国建筑[601668]、中国铁建[601186]</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <dv-decoration-2 class="chart-container-line" />
+        <div class="chart-container-right">
+          <dv-decoration-9 class="chart-container-right-strategy">策略三</dv-decoration-9>
+          <div class="chart-container-right-con">
+            <div class="chart-container-right-con-item">
+              <div>当前持仓</div>
+              <div>
+                <div>概念板块：贵州茅台[600519]、山西汾酒[600809]、 五粮液[000858]</div>
+                <div>军工板块：航天晨光[600501]、东安动力[600178]、航发科技[600391]...</div>
+              </div>
+            </div>
+            <div class="chart-container-right-con-item">
+              <div>今日新增</div>
+              <div>
+                <div>基建板块：中国交建[601800]、中国建筑[601668]、中国铁建[601186]</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <dv-decoration-2 class="chart-container-line" />
+
+        <div class="chart-container-right">
+          <dv-border-box-8 class="chart-container-right-img">
+            <img src="https://www.ditingai.com/skin/assets/common/images/dt-logo.png">
+          </dv-border-box-8>
+          <div style="text-align: center;">
+            让投资变得更简单
+            <dv-decoration-3 style="width:220px;height:30px;" />
+          </div>
+
+          <a href="https://www.ditingai.com/" target="_blank">
+            <dv-decoration-11 style="width:200px;height:60px;">自营平台</dv-decoration-11>
+          </a>
+        </div>
       </dv-border-box-11>
     </div>
   </div>
@@ -25,7 +95,7 @@
 
 <script>
 export default {
-  name: 'TopHeader',
+  name: 'Content',
   data() {
     return {
       config: {
@@ -69,7 +139,8 @@ export default {
         evenRowBGC: 'rgba(10, 29, 50, 0.8)'
       }
     }
-  }
+  },
+  methods: {}
 }
 </script>
 
@@ -78,23 +149,70 @@ export default {
   flex: 1;
   display: flex;
   flex-direction: column;
-  .left-chart {
+  .chart {
     display: flex;
     height: 100%;
     &-container {
       padding: 70px 30px 30px;
       display: flex;
       box-sizing: border-box;
-      &-title {
-        margin: 10px 0;
-        width: 110px;
+      &-left {
+        &-title {
+          margin: 10px 0;
+          width: 110px;
+          height: 20px;
+        }
+        &-title:first-child {
+          margin-top: 0;
+        }
+        &-table {
+          height: 132px;
+        }
+      }
+      &-right {
+        display: flex;
+        align-items: center;
+        &-strategy {
+          height: 80px;
+          width: 80px;
+        }
+        &-con {
+          display: flex;
+          flex-direction: column;
+          padding-left: 10px;
+          width: 86%;
+          &-item {
+            flex-grow: 1;
+            display: flex;
+            // align-items: center;
+            margin-bottom: 10px;
+            > div:first-child {
+              font-size: 22px;
+              color: #ea6027;
+              width: 60px;
+              text-align: center;
+            }
+            div {
+              color: #67e0e3;
+            }
+          }
+        }
+        &-img {
+          width: 160px;
+          height: 160px;
+          position: relative;
+          img {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            transform: translate(-50%, -50%);
+            width: 140px;
+            height: 140px;
+          }
+        }
+      }
+      &-line {
         height: 20px;
-      }
-      &-title:first-child {
-        margin-top: 0;
-      }
-      &-table {
-        height: 132px;
       }
     }
   }
